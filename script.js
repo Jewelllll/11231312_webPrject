c0cec242e528203bad532907105435faeaa51641
Jewelllll <163641714+Jewelllll@users.noreply.github.com>
2024-05-31 23:37:39 +0800

    Update script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+var Button = document.querySelector("button");
+
+function BMI() {
+  location.href = "BMI.html";
+}
+function phy() {
+
+}
+function foodChoice() {
+  location.href = "foodChoice.html";
+}
+function physics() {
+  location.href = "physics.html";
+}
+
+
